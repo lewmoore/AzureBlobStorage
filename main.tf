b627bb10a967dafdc82516bde3f87913908fe3be
lewmoore <lewis.moore2707@gmail.com>
2018-08-02 13:56:06 +0100

    Add storage container resource

--- main.tf
+++ main.tf
@@ -14,3 +14,10 @@ resource "azurerm_storage_account" "AzureSA" {
   account_tier = "Standard"
   account_replication_type = "GRS"
 }
+
+resource "azurerm_storage_container" "container1" {
+  name = "container1"
+  resource_group_name = "${azurerm_resource_group.AzureRG.name}"
+  storage_account_name = "${azurerm_storage_account.AzureSA.name}"
+  container_access_type = "private"
+}
