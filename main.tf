1711ac82406bb2fe841fbb3a703f45c3304d76a1
lewmoore <lewis.moore2707@gmail.com>
2018-08-02 13:48:18 +0100

    Add storage account resource

--- main.tf
+++ main.tf
@@ -6,3 +6,11 @@ resource "azurerm_resource_group" "AzureRG" {
   name = "resource-group"
   location = "${var.location}"
 }
+
+resource "azurerm_storage_account" "AzureSA" {
+  name = "createnewstorageaccount"
+  resource_group_name = "${azurerm_resource_group.AzureRG.name}"
+  location = "${var.location}"
+  account_tier = "Standard"
+  account_replication_type = "GRS"
+}
